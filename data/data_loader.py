a671d92899ee76338fc4f6f1a9e4d963fc8ad7f9
silviam <zmvs671@gmail.com>
2025-03-16 22:42:46 -0600

    FEAT(data): cambios en data_loader.py

--- data/data_loader.py
+++ data/data_loader.py
@@ -3,69 +3,79 @@ import pandas as pd
 import mysql.connector
 from datetime import datetime, timedelta
 
-#Verificar que la variable de entorno esté configurada
-db_password = os.getenv("DB_PASSWORD")
+def cargar_datos(txt_file, csv_file):
+    """
+    Procesa el archivo .txt, lo convierte a formato .csv y lo carga en la base de datos MySQL
+    Argumentos:
+        txt_file(str): Ruta del archivo .txt con los datos de viento solar
+        csv_file(str): Ruta del archivo .csv donde se guardarán los datos procesados
+    """
+    #Verifica que la variable de entorno DB_PASSWORD esté configurada
+    db_password = os.getenv("DB_PASSWORD")
+    if not db_password:
+        raise ValueError("La variable de entorno DB_PASSWORD para usar SQL no está configurada. Establezca la variable de entorno antes de ejecutar el código.")
 
-if not db_password:
-	raise ValueError("La variable de entorno DB_PASSWORD para usar SQL no está configurada.Establezca la variable de entorno antes de ejecutar el código.")
+    #Leer el archivo .txt con pandas separando por espacios
+    df = pd.read_csv(txt_file, delim_whitespace=True)
 
-#Procesamiento del archivo .txt de los datos originales y convertirlo a .csv
-txt_file = "data/20240507_20240512.txt"
-csv_file = "data/viento_solar.csv"
+    #Definir nombres de columnas
+    df.columns = ["year", "day_of_year", "hour_of_day", "plasma_temperature", "proton_density", "plasma_speed"]
 
-#Leer el archivo .txt, saltando las líneas de encabezado y separando por espacios
-df = pd.read_csv(txt_file, delim_whitespace=True, header=None)
+    #Convertir la fecha en formato YYYY-MM-DD HH:MM:SS
+    df['fecha'] = df.apply(lambda row: datetime(row["year"], 1, 1) + timedelta(days=row["day_of_year"] - 1, hours=row["hour_of_day"]), axis=1)
 
-#Definir nombres de columnas
-df.columns = ["year", "dayOfYear", "houOfDay", "plasmaTemperature", "protonDensity", "plasmaSpeed"]
+    #Guardar DataFrame como .csv
+    df = df[["fecha", "plasma_temperature", "proton_density", "plasma_speed"]]
+    df.to_csv(csv_file, index=False)
 
-#Convertir la fecha en formato YYYY-MM-DD HH:MM:SS
-df['fecha'] = df.apply(lambda row: datetime(row["year"], 1, 1) + timedelta(days=row["dayOfYear"] - 1, hours=row["hour"]), axis=1)
+    print("Archivo .csv generado exitosamente.")
 
-#Guardar DataFrame como .csv
-df = df[["fecha", "solarWindSpeed", "solarWindDensity", "solarWindTemperature"]
-df.to_csv(csv_file, index=False)
+    #Cargar el archivo .csv a MySQL
+    cargar_a_sql(csv_file, db_password)
 
-print("Archivo .csv generado exitosamente.")
-
-#Cargar el archivo .csv a MySQL
-
-#Conexión a la base de datos MySQL
-db_connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password=db_password,
-    database="viento_solar_db"
-)
-
-cursor = db_connection.cursor()
-
-#Crear la tabla si no existe
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS viento_solar (
-        fecha DATETIME,
-        plasmaTemperature FLOAT,
-        protonDensity FLOAT,
-        plasmaSpeed FLOAT
+def cargar_a_sql(csv_file, db_password):
+    """
+    Carga los datos desde un archivo .csv a la base de datos MySQL
+    Argumentos
+        csv_file(str): Ruta del archivo .csv que contiene los datos
+        db_password(str): Contraseña de la base de datos MySQL
+    """
+    #Conexión a la base de datos MySQL
+    db_connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password=db_password,
+        database="viento_solar_db"
     )
-""")
 
-#Cargar los datos desde el archivo csv a MySQL 
-load_sql = """
-    LOAD DATA LOCAL INFILE '""" + csv_file + """'
-    INTO TABLE viento_solar
-    FIELDS TERMINATED BY ',' 
-    LINES TERMINATED BY '\\n'
-    IGNORE 1 ROWS
-    (fecha, plasmaTemperature, protonDensity, plasmaSpeed)
-"""
+    cursor = db_connection.cursor()
 
-cursor.execute(load_sql)
-db_connection.commit()
+    #Crear la tabla si no existe
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS viento_solar (
+            fecha DATETIME,
+            plasma_temperature FLOAT,
+            proton_density FLOAT,
+            plasma_speed FLOAT
+        )
+    """)
 
-print("Datos cargados a MySQL exitosamente.")
+    #Cargar los datos desde el archivo .csv a MySQL
+    load_sql = """
+        LOAD DATA LOCAL INFILE '""" + csv_file + """'
+        INTO TABLE viento_solar
+        FIELDS TERMINATED BY ',' 
+        LINES TERMINATED BY '\\n'
+        IGNORE 1 ROWS
+        (fecha, plasma_temperature, proton_density, plasma_speed)
+    """
 
-#Cerrar la conexión
-cursor.close()
-db_connection.close()
+    cursor.execute(load_sql)
+    db_connection.commit()
+
+    print("Datos cargados a MySQL exitosamente")
+
+    #Cerrar la conexión
+    cursor.close()
+    db_connection.close()
 
